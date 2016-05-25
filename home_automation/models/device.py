e878b5fcc7304894594e0f6fadf7c37998aa13a6
Vidit Jain <jain.vidit@outlook.com>
2016-05-25 23:53:56 +0530

    Adding device name to model
    Signed-off-by: Vidit Jain <jain.vidit@outlook.com>

--- home_automation/models/device.py
+++ home_automation/models/device.py
@@ -6,6 +6,7 @@ class Device(db.Model):
     __tablename__ = 'devices'
     id = db.Column(db.Integer,
                    primary_key=True)
+    name = db.Column(db.String, nullable = False)
     status = db.Column(db.Boolean,
                      nullable=False)
     location_id = db.Column(db.Integer,
@@ -14,8 +15,10 @@ class Device(db.Model):
 
     def __init__(self,
                  status=None,
+                 name = None,
                  location_id=None):
         self.status = status
+        self.name = name
         self.location_id = location_id
 
     def __repr__(self):
@@ -25,6 +28,7 @@ class Device(db.Model):
     def serialize(self):
         """Return object data in easily serializeable format"""
         return {'id': self.id,
+                'name': self.name,
                 'status': self.status,
                 'location_id': self.location_id
                 }
